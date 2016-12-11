110a862fa48a0879aafb62d272ef7f65003f2000
jy03147937 <SESA275378@W7CN026XAR00602.apa.gad.schneider-electric.com>
2016-12-11 10:49:15 +0800

    To support passing float type parameter to AddDays
    1.为支持此功能，算法上将日期先转为秒，再转回日期。
    2.同时增加了其他几个常用方法。

--- src/Controls/DateTime.java
+++ src/Controls/DateTime.java
@@ -13,14 +13,16 @@ public class DateTime {
 		DateTime dt = new DateTime();
 		
 		try {
-			dt = new DateTime("2016-11-23 02:23:1");
+			dt = new DateTime("2017-01-15");
+			System.out.println(dt.getDayOfYear());
+			DateTime newDt = dt.addDays(26.54f);
 			
 			System.out.println(dt.toString());
-			
-			DateTime newDt = dt.addDays(7.5f);
 			
 			System.out.println(newDt.toString());
-			System.out.println(dt.toString());
+			
+			System.out.println(convertYoungSecondsToDateTime(3600 * 24 * 365 * 6 + 8 * 3600 + 45 * 60 + 64));
+
 		} catch (DatetimeFormatException e) {
 			
 			e.printStackTrace();
@@ -28,17 +30,18 @@ public class DateTime {
 		}
 	}
 	
-	int year = 1988;
+	private int year = 1988;
 	
-	int month = 7;
+	private int month = 7;
 	
-	int day = 27;
+	private int day = 27;
 	
-	int hour = 7;
+	private int hour = 7;
 	
-	int minute = 10;
+	private int minute = 10;
 	
-	int second = 0;
+	private int second = 0;
+	
 	
 	DateTime () {
 		Date dt = new Date();
@@ -54,7 +57,7 @@ public class DateTime {
 	}
 	
 	DateTime (String datetime) throws DatetimeFormatException {
-		String reg = "\\b\\d{1,4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}\\b";
+		String reg = "\\b\\d{1,4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}\\b|\\b\\d{1,4}-\\d{1,2}-\\d{1,2}\\b";
 		Boolean isMatch = datetime.matches(reg);
 		if(!isMatch) {
 			throw new DatetimeFormatException("The input string is not the format: yy-mm-ss h-m-s");
@@ -62,12 +65,12 @@ public class DateTime {
 		
 		String[] strA = datetime.split("\\D");
 
-		this.year = Integer.parseInt(strA[0]);
-		this.month = Integer.parseInt(strA[1]);
-		this.day = Integer.parseInt(strA[2]);
-		this.hour = Integer.parseInt(strA[3]);
-		this.minute = Integer.parseInt(strA[4]);
-		this.second = Integer.parseInt(strA[5]);
+		this.year = strA.length > 0 ? Integer.parseInt(strA[0]) : 0;
+		this.month = strA.length > 1 ? Integer.parseInt(strA[1]) : 0;
+		this.day = strA.length > 2 ? Integer.parseInt(strA[2]) : 0;
+		this.hour = strA.length > 3 ? Integer.parseInt(strA[3]) : 0;
+		this.minute = strA.length > 4 ? Integer.parseInt(strA[4]) : 0;
+		this.second = strA.length > 5 ? Integer.parseInt(strA[5]) : 0;
 		
 	}
 	
@@ -93,6 +96,25 @@ public class DateTime {
 		return dt;
 	}
 	
+	public int Year () {
+		return this.year;
+	}
+	public int Month () {
+		return this.month;
+	}
+	public int Day () {
+		return this.day;
+	}
+	public int Hour () {
+		return this.hour;
+	}
+	public int Minute () {
+		return this.minute;
+	}
+	public int Second () {
+		return this.second;
+	}
+	
 	public String toString() {
 		StringBuffer str = new StringBuffer();
 		
@@ -111,30 +133,203 @@ public class DateTime {
 		return str.toString();
 	}
 	
+	public Boolean isEquals (DateTime dateTime) {
+		if (dateTime.Year() == this.year && 
+			dateTime.Month() == this.month &&
+			dateTime.Day() == this.day &&
+			dateTime.Hour() == this.hour &&
+			dateTime.Minute() == this.minute &&
+			dateTime.Second() == this.second) 
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean isBigger (DateTime dateTime) {
+		if(dateTime.Year() > this.year) {
+			return false;
+		} else if (dateTime.Year() < this.year) {
+			return true;
+		}
+		if(dateTime.Month() > this.month) {
+			return false;
+		} else if (dateTime.Month() < this.month) {
+			return true;
+		}
+		if(dateTime.Day() > this.day) {
+			return false;
+		} else if (dateTime.Day() < this.day) {
+			return true;
+		}
+		if(dateTime.Hour() > this.hour) {
+			return false;
+		} else if (dateTime.Hour() < this.hour) {
+			return true;
+		}
+		if(dateTime.Minute() > this.minute) {
+			return false;
+		} else if (dateTime.Minute() < this.minute) {
+			return true;
+		}
+		if(dateTime.Second() > this.second) {
+			return false;
+		} else if (dateTime.Second() < this.second) {
+			return true;
+		}
+		return true;
+	}
+	
 	public DateTime addDays(float days) throws DatetimeFormatException {
 		
-		int daysInt = (int)Math.floor(days);	
-		this.day += daysInt;
+		float oriSeconds = getYoungSeconds(this);
+	
+		DateTime oriDateTime = convertYoungSecondsToDateTime(oriSeconds);
 		
-		float daysDecimal = days - daysInt;
-		int hoursInt = (int)Math.floor(daysDecimal * 24);
-		this.hour += hoursInt;
+		float addedSeconds = oriSeconds + days * 3600 * 24;
 		
-		float hoursDecimal = daysDecimal * 24 - hoursInt;
-		int minutesInt = (int)Math.floor(hoursDecimal * 60);
-		this.minute += minutesInt;
+		DateTime addedDateTime = convertYoungSecondsToDateTime(addedSeconds);
 		
-		float minutesDecimal = hoursDecimal * 60 - minutesInt;
-		int secondsInt = (int)Math.floor(minutesDecimal * 60);
-		this.second += secondsInt;
+		return addedDateTime;
+	}
+	
+	public DateTime addHours(float hours) throws DatetimeFormatException {
 		
-		DateTime result = new DateTime(this.toString());
+		float oriSeconds = getYoungSeconds(this);
 		
-		this.day -= daysInt;
-		this.hour -= hoursInt;
-		this.minute -= minutesInt;
-		this.second -= secondsInt;
+		float addedSeconds = oriSeconds + hours * 3600;
 		
-		return result;
+		DateTime addedDateTime = convertYoungSecondsToDateTime(addedSeconds);
+		
+		return addedDateTime;
+	}
+	
+	private float getYoungSeconds(DateTime dateTime) throws DatetimeFormatException{
+		
+		DateTime youngDateTime = new DateTime("1988-01-01 00:00:00");
+		
+		int gapYears = dateTime.Year() - youngDateTime.Year();
+		
+		int totalSeconds = gapYears * 365 * 24 * 3600;
+		
+		int leapYearsNum = gapYears % 4 == 0 ? (int)Math.floor(gapYears / 4) : (int)Math.floor(gapYears / 4) + 1;
+		
+		totalSeconds += leapYearsNum * 24 * 3600;
+		
+		int dayOfTheYear = dateTime.getDayOfYear();
+		
+		totalSeconds += (dayOfTheYear - 1) * 24 * 3600;
+		
+		totalSeconds += dateTime.Hour() * 3600;
+		
+		totalSeconds += dateTime.Minute() * 60;
+		
+		totalSeconds += dateTime.Second();
+		
+		return totalSeconds;
+	}
+	
+	private static DateTime convertYoungSecondsToDateTime(float seconds) throws DatetimeFormatException {
+		
+		DateTime youngDateTime = new DateTime("1988-01-01 00:00:00");
+		
+		float leftSeconds = seconds;
+		
+		int year = youngDateTime.Year();
+		
+		while ((year % 4 == 0 && leftSeconds >= 366 * 3600 * 24) || (year % 4 != 0 && leftSeconds >= 365 * 3600 * 24)) {
+			
+			if (year % 4 == 0) {
+				year++;
+				leftSeconds -= 366 * 3600 * 24;
+			} else {
+				year++;
+				leftSeconds -= 365 * 3600 * 24;
+			}
+		}
+		
+		int month = youngDateTime.Month();
+		
+		while (leftSeconds >= getDaysInMonth(month, year) * 3600 * 24) {
+			leftSeconds -= getDaysInMonth(month, year) * 3600 * 24;
+			
+			month++;
+		}
+		
+		int day = youngDateTime.Day();
+		
+		day += (int)(leftSeconds / (24 * 3600));
+		
+		leftSeconds -= ((int)(leftSeconds / (24 * 3600))) * 24 * 3600;
+		
+		int hour = youngDateTime.Hour();
+		
+		hour += (int)(leftSeconds / 3600);
+		
+		leftSeconds -= ((int)(leftSeconds / 3600)) * 3600 ;
+		
+		int minute = youngDateTime.Minute();
+		
+		minute += (int)(leftSeconds / 60);
+		
+		leftSeconds -= ((int)(leftSeconds / 60)) * 60;
+		
+		int second = youngDateTime.Second();
+		
+		second += (int)leftSeconds;
+		
+		return new DateTime(year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second);
+	}
+	
+	public int getDayOfYear() {
+		
+		int month = this.Month();
+		
+		int totalDays = 0;
+		
+		for(int i = 1; i < month; i++) {
+			totalDays += getDaysInMonth(i, this.Year());
+		}
+		
+		totalDays += this.Day();
+
+		return totalDays;
+	}
+	
+	private static int getDaysInMonth(int month, int year) {
+		int days = 0;
+		
+		Boolean isLeapYear = false;
+		if (year % 4 == 0) {
+			isLeapYear = true;
+		}
+		switch(month) {
+			case 1: days = 31;
+				break;
+			case 2: days = isLeapYear ? 29 : 28;
+				break;
+			case 3: days = 31;
+				break;
+			case 4: days = 30;
+				break;
+			case 5: days = 31;
+				break;
+			case 6: days = 30;
+				break;
+			case 7: days = 31;
+				break;
+			case 8: days = 31;
+				break;
+			case 9: days = 30;
+				break;
+			case 10: days = 31;
+				break;
+			case 11: days = 30;
+				break;
+			case 12: days = 31;
+				break;
+		}
+		
+		return days;
 	}
 }
